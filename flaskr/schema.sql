f979a3a1732fddb1d14df2055799151b6599d564
Unknown <jagustinbassi@gmail.com>
2020-02-04 11:04:22 -0300

    Create the feature of like/dislike without user-post association
    Create tables for likes/dislikes and associate them by user and post
    
    
    Create querys to visualize the likes/dislikes and pass them to templates
    
    
    Add functionality to add/remove each like/dislike per user

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS likes;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,4 +15,20 @@ CREATE TABLE post (
   title TEXT NOT NULL,
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (post_id) REFERENCES post (id)
+);
+
+CREATE TABLE dislikes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (post_id) REFERENCES post (id)
 );
